dd29548cbead39a83b02908d17bb6c26086a698a
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 20:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 const [red, orange, yellow, green, blue, indigo, violet] = colors
 
-const [r, o, y, g, b, v] = colors 
+const [r, o, y, g, b, ,v] = colors 
 
 
 const muppet = {

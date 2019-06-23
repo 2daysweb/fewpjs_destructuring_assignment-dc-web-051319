ed1f789a638755a562c1b74fc18654755cd85ce4
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 20:32:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,23 +45,4 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
-// Strings
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-
-
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner

7211bedcee728ba6133f146936f899211b062e1b
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 20:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const farmAnimals = 'cow horse sheep pig chicken'
-const moo = farmAnimals.split(" ")[0]
-const neigh = farmAnimals.split(" ")[1]
-const baa = farmAnimals.split(" ")[2]
-const oink = farmAnimals.split(" ")[3]
-const cluck = farmAnimals.split(" ")[4]
+// const farmAnimals = 'cow horse sheep pig chicken'
+// const moo = farmAnimals.split(" ")[0]
+// const neigh = farmAnimals.split(" ")[1]
+// const baa = farmAnimals.split(" ")[2]
+// const oink = farmAnimals.split(" ")[3]
+// const cluck = farmAnimals.split(" ")[4]
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
